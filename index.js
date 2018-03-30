3ca44ce4e56d80814c6fe1fcb9234831f926b0f0
Pavel Ladygin <me@digitorum.ru>
2018-03-30 18:23:43 +0300

    Полчение простых форм слов.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 require('./utilities/ispell.js').loadDictionaries([
     './ispell-dictionaries/russian.dict'
-]).then(function (tree) {
-    console.log("словари загружены");
+], [
+    './ispell-dictionaries/russian.aff'
+]).then(function (trees) {
+    var text = "Эпитеты как бы это ни казалось парадоксальным иллюстрируют метафоричные символы";
+
+    console.log(text);
+    text.toLowerCase().split(" ").forEach(function (word) {
+        console.log(" >", word);
+        trees.affixes.getWordPossibleSimpleForms(word).forEach(function (possibility) {
+            if (trees.words.findWord(possibility)) {
+                console.log("  <", possibility);
+            }
+        });
+    });
 });
--- utilities/ispell.js
+++ utilities/ispell.js
@@ -1,7 +1,7 @@
 var readline = require('readline');
 var fs = require('fs');
 
-//#region LetterNode
+//#region TreeNode
 
 /**
  * Нода дерева.
@@ -9,7 +9,7 @@ var fs = require('fs');
  * @param {any} value
  * @constructor
  */
-function LetterNode(value) {
+function TreeNode(value) {
     this.value = value;
     this.children = [];
 }
@@ -19,32 +19,32 @@ function LetterNode(value) {
  * 
  * @type {Array?}
  */
-LetterNode.prototype.children = null;
+TreeNode.prototype.children = null;
 
 /**
  * Значение ноды
  * 
  * @type {String}
  */
-LetterNode.prototype.value = null;
+TreeNode.prototype.value = null;
 
 /**
  * Дополнтильные данные для ноды
  * 
  * @type {object}
  */
-LetterNode.prototype.data = null;
+TreeNode.prototype.data = null;
 
 /**
  * Добавить ребенка к ноде.
  * 
  * @param {any} value
  */
-LetterNode.prototype.addChildren = function (value) {
+TreeNode.prototype.addChildren = function (value) {
     var node = this.find(value);
 
     if (!node) {
-        node = new LetterNode(value);
+        node = new TreeNode(value);
         this.children.push(node);
     }
     return node;
@@ -55,8 +55,17 @@ LetterNode.prototype.addChildren = function (value) {
  * 
  * @param {object} data
  */
-LetterNode.prototype.setData = function (data) {
+TreeNode.prototype.setData = function (data) {
     this.data = data;
+}
+
+/**
+ * Данные ноды
+ * 
+ * @returns {Object}
+ */
+TreeNode.prototype.getData = function () {
+    return this.data || {};
 }
 
 /**
@@ -64,7 +73,7 @@ LetterNode.prototype.setData = function (data) {
  * 
  * @param {any} level
  */
-LetterNode.prototype.dumpStructure = function (level) {
+TreeNode.prototype.dumpStructure = function (level) {
     var str = "";
 
     if (!level) {
@@ -81,9 +90,9 @@ LetterNode.prototype.dumpStructure = function (level) {
  * Найти ноду среди дочерних
  * 
  * @param {any} value
- * @returns {LetterNode}
+ * @returns {TreeNode}
  */
-LetterNode.prototype.find = function (value) {
+TreeNode.prototype.find = function (value) {
     return this.children.filter(function (item) {
         return item.value == value;
     })[0] || null;
@@ -99,11 +108,11 @@ LetterNode.prototype.find = function (value) {
  * @constructor
  */
 function WordsTree() {
-    LetterNode.prototype.constructor.apply(this, arguments);
+    TreeNode.prototype.constructor.apply(this, arguments);
 }
 
 // Полностью наследуемся от ноды.
-WordsTree.prototype = Object.create(LetterNode.prototype);
+WordsTree.prototype = Object.create(TreeNode.prototype);
 WordsTree.prototype.constructor = WordsTree;
 
 /**
@@ -128,9 +137,9 @@ WordsTree.prototype.addString = function (str, data) {
  * Найти строку в дереве.
  * 
  * @param {any} str
- * @returns {LetterNode}
+ * @returns {TreeNode}
  */
-WordsTree.prototype.findString = function (str) {
+WordsTree.prototype.findWord = function (str) {
     var node = this;
 
     if (typeof (str) == "string") {
@@ -139,7 +148,88 @@ WordsTree.prototype.findString = function (str) {
     while (node !== null && str.length) {
         node = node.find(str.shift());
     }
-    return node;
+    if (node && node.data) {
+        // Если привязаны данные, значит это слово, а не промежуточная нода.
+        return node;
+    }
+    return null;
+}
+
+//#endregion
+
+//#region AffixesTree
+
+function AffixesTree() {
+    TreeNode.prototype.constructor.apply(this, arguments);
+}
+
+// Полностью наследуемся от ноды.
+AffixesTree.prototype = Object.create(TreeNode.prototype);
+AffixesTree.prototype.constructor = AffixesTree;
+
+/**
+ * Добавить аффикс в дерево
+ * 
+ */
+AffixesTree.prototype.addAffix = function (modes, pattern, replacement, affix) {
+    var node = this.addChildren(affix.toLowerCase());
+    var data = node.getData();
+
+    if (!data.modes) {
+        data.modes = {};
+    }
+    modes.forEach(function (item) {
+        if (!data.modes[item]) {
+            data.modes[item] = [];
+        }
+        data.modes[item].push({
+            pattern: pattern,
+            regexp: new RegExp(pattern + "$", "i"),
+            replacement: (replacement || "").toLowerCase()
+        });
+    });
+    node.setData(data);
+}
+
+/**
+ * Получить возможные навальные формы слова
+ * 
+ * @param {any} word
+ * @returns {Array}
+ */
+AffixesTree.prototype.getWordPossibleSimpleForms = function (word) {
+    var total = this.children.length;
+    var wordLen = word.length;
+    var result = [
+        word // Поумолчанию включаем слово в потенциально возможные словоформы.
+    ];
+
+    for (var i = 0; i < total; ++i) {
+        var node = this.children[i];
+        var data = node.getData();
+        var affix = node.value;
+        var affixLen = affix.length;
+
+        if (wordLen < affixLen) {
+            continue;
+        }
+        
+        if (word.substr(wordLen - affixLen, affixLen) == affix) {
+            for (var j in data.modes) {
+                data.modes[j].forEach(function (item) {
+                    var possible = word.substr(0, wordLen - affixLen) + (item.replacement || "");
+
+                    if (item.regexp.test(possible)) {
+                        result.push(possible);
+                    }
+                });
+            }
+        }
+    }
+    // Возвращаем только уникальные значения.
+    return result.filter(function (value, index, self) {
+        return self.indexOf(value) === index;
+    });
 }
 
 //#endregion
@@ -149,42 +239,113 @@ WordsTree.prototype.findString = function (str) {
  * 
  * @constructor
  */
-function Ispell() {
+function Ispell() {}
 
+/**
+ * Построчно обработать файл
+ * 
+ * @param {any} list
+ * @param {any} linePerform
+ * @returns {Promise}
+ */
+Ispell.prototype.loadFilesListByLine = function (list, linePerform) {
+    return new Promise(function (resolve) {
+        var done = 0;
+        var total = list ? list.length : 0;
+
+        if (total) {
+            for (var i = 0; i < total; ++i) {
+                var dictionary = list[i];
+                var lineReader = readline.createInterface({
+                    input: fs.createReadStream(dictionary)
+                });
+
+                lineReader.on('line', linePerform).on('close', () => {
+                    done++;
+                    if (total == done) {
+                        resolve()
+                    }
+                });
+            }
+        } else {
+            resolve();
+        }
+    });
 }
 
 /**
  * Загрузить словари
  * 
- * @param {any} list
+ * @param {any} words
+ * @param {any} affixes
  * @returns {Promise}
  */
-Ispell.prototype.loadDictionaries = function (list) {
-    var tree = new WordsTree();
+Ispell.prototype.loadDictionaries = function (words, affixes) {
+    var that = this;
+    var totalLists = arguments.length;
+    var wordsTree = new WordsTree();
+    var affixesTree = new AffixesTree();
+    var affixModes = null;
 
     return new Promise(function (resolve) {
-        var total = list.length;
         var done = 0;
 
-        for (var i = 0; i < total; ++i) {
-            var dictionary = list[i];
-            var lineReader = readline.createInterface({
-                input: fs.createReadStream(list[i])
-            });
-
-            lineReader.on('line', function (line) {
-                var data = line.split("/");
-
-                tree.addString(data[0], {
-                    "flags": (data[1] || "").split("")
+        /**
+         * Зарезолвить промис в момент когда все списки будут загружены
+         */
+        function doResolve() {
+            done++;
+            if (totalLists == done) {
+                resolve({
+                    words: wordsTree,
+                    affixes: affixesTree
                 });
-            }).on('close', () => {
-                done++;
-                if (total == done) {
-                    resolve(tree);
-                }
-            });
+            }
         }
+
+        // Форируем дерево слов.
+        that.loadFilesListByLine(words, function (line) {
+            var data = line.split("/");
+
+            wordsTree.addString(data[0], {
+                "flags": (data[1] || "").split("")
+            });
+        }).then(doResolve);
+
+        // Формируем дерево аффиксов.
+        that.loadFilesListByLine(affixes, function (line) {
+            line = line.replace(/^[\s\t]+|[\s\t]+$/g, '');
+
+            if (line != "" && line[0] != "#") {
+                // Проверяем есть ли признак флага
+                var tryFlagFind = line.match(/^flag[\s\t]+\*([a-z])+:/i);
+
+                if (tryFlagFind) {
+                    affixModes = tryFlagFind[1].split("");
+                } else {
+                    if (affixModes !== null) {
+                        // Проверяем есть ли правила для замены `... > -A,B`
+                        var tryAffixReplace = line.match(/^(.*?)[\s\t]+>[\s\t]+\-([^\s]+),([^\s]+)/i);
+
+                        if (tryAffixReplace) {
+                            affixModes.forEach(function () {
+                                affixesTree.addAffix(affixModes, tryAffixReplace[1].replace(/[\s\t]*/g, ''), tryAffixReplace[2], tryAffixReplace[3]);
+                            });
+                        } else {
+                            // Проверяем есть ли правила добпавления `... > A`
+                            var tryAffixAdd = line.match(/^(.*?)[\s\t]+>[\s\t]+([^\s]+)/i);
+
+                            if (tryAffixAdd) {
+                                affixModes.forEach(function () {
+                                    affixesTree.addAffix(affixModes, tryAffixAdd[1].replace(/[\s\t]*/g, ''), null, tryAffixAdd[2]);
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }).then(doResolve);
+
     });
 }
 
